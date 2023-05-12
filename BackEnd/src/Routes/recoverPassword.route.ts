5c430da0c5a98e1e544da6b074a46c9e733b90ef
Giancarlo Pessatti <106692510+GiancarloPessatti@users.noreply.github.com>
2023-05-12 13:29:40 -0300

    Delete BackEnd directory

--- BackEnd/src/Routes/recoverPassword.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { recoverpasswordController } from "../Controllers/login.controller";
-
-const recoverRouter = Router();
-
-recoverRouter.post(`/api/recoverpassword`, recoverpasswordController);
-
-export default recoverRouter;
